7f1b84bfde3b6693fee9375e0531206f45575f73
Anggie <134072088+AnggieRM@users.noreply.github.com>
2023-10-05 20:59:22 -0500

    Agregar archivos de proyecto.

---
+++ ElectroHouse/formProductos.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace ElectroHouse
+{
+    partial class formProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            textBox4 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button3 = new Button();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackgroundImageLayout = ImageLayout.None;
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(57, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(494, 326);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(47, 167);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(159, 23);
+            dateTimePicker1.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(294, 59);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(131, 23);
+            textBox6.TabIndex = 22;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(304, 41);
+            label9.Name = "label9";
+            label9.Size = new Size(69, 15);
+            label9.TabIndex = 21;
+            label9.Text = "Proveedor:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(204, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(113, 30);
+            label8.TabIndex = 20;
+            label8.Text = "Productos";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.AppWorkspace;
+            button2.Location = new Point(245, 266);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 31);
+            button2.TabIndex = 19;
+            button2.Text = "Limpiar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(47, 219);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(109, 23);
+            textBox5.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(305, 93);
+            label7.Name = "label7";
+            label7.Size = new Size(105, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Tipo de producto:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(57, 201);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Precio:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(294, 111);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(126, 23);
+            comboBox2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(122, 266);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 31);
+            button1.TabIndex = 10;
+            button1.Text = "Registrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(294, 219);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(116, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(47, 113);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(142, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(47, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(142, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(294, 165);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(134, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(305, 201);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Cantidad:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(305, 147);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Estado:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(57, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Fecha de llegada:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(57, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(57, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo:";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.AppWorkspace;
+            button3.Location = new Point(586, 376);
+            button3.Name = "button3";
+            button3.Size = new Size(84, 31);
+            button3.TabIndex = 25;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            listView1.Location = new Point(43, 368);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(519, 157);
+            listView1.TabIndex = 24;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Codigo";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Nombre";
+            columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Precio";
+            columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Estado";
+            columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Proveedor";
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.AppWorkspace;
+            button4.Location = new Point(551, 552);
+            button4.Name = "button4";
+            button4.Size = new Size(84, 31);
+            button4.TabIndex = 25;
+            button4.Text = "Salir";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // formProductos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(651, 595);
+            Controls.Add(button4);
+            Controls.Add(groupBox1);
+            Controls.Add(listView1);
+            Name = "formProductos";
+            Text = "formProductos";
+            Load += formProductos_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button3;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox6;
+        private Label label9;
+        private Label label8;
+        private Button button2;
+        private TextBox textBox5;
+        private Label label7;
+        private Label label6;
+        private ComboBox comboBox2;
+        private Button button1;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private Button button4;
+    }
+}
---
+++ ElectroHouse/formProveedor.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ElectroHouse
+{
+    partial class formProveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            button3 = new Button();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackgroundImageLayout = ImageLayout.None;
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(41, 24);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(521, 279);
+            groupBox1.TabIndex = 13;
+            groupBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(302, 178);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(164, 23);
+            textBox4.TabIndex = 25;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(324, 160);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 15);
+            label7.TabIndex = 24;
+            label7.Text = "Telefono";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(198, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 30);
+            label2.TabIndex = 23;
+            label2.Text = "Proveedores";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.AppWorkspace;
+            button2.Location = new Point(293, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 31);
+            button2.TabIndex = 22;
+            button2.Text = "Limpiar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(55, 178);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(164, 23);
+            textBox5.TabIndex = 21;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(77, 160);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 15);
+            label6.TabIndex = 20;
+            label6.Text = "R.U.C:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(75, 104);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 15);
+            label5.TabIndex = 17;
+            label5.Text = "Nombre:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(302, 69);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(164, 23);
+            textBox6.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(155, 222);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 31);
+            button1.TabIndex = 10;
+            button1.Text = "Registrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(302, 122);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(171, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(55, 122);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(149, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(129, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(322, 104);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Direccion:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(324, 51);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Ciudad";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(75, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo:";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.AppWorkspace;
+            button3.Location = new Point(540, 518);
+            button3.Name = "button3";
+            button3.Size = new Size(91, 31);
+            button3.TabIndex = 26;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            listView1.Location = new Point(41, 325);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(495, 181);
+            listView1.TabIndex = 25;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Codigo";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Nombre";
+            columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Telefono";
+            columnHeader3.Width = 120;
+            // 
+            // formProveedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(652, 561);
+            Controls.Add(button3);
+            Controls.Add(listView1);
+            Controls.Add(groupBox1);
+            Name = "formProveedor";
+            Text = "formProveedor";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label2;
+        private Button button2;
+        private TextBox textBox5;
+        private Label label6;
+        private Label label5;
+        private TextBox textBox6;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label1;
+        private Button button3;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+    }
+}
